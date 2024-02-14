d7acb74f40755cd7a5854a464742267b5d382103
AnnaPatricia <triciaentrada@gmail.com>
2024-02-14 17:18:43 +0800

    activities 1-2

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,12 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+names = {first_name: 'Anna', middle_name: 'Paguntalan', last_name: 'Entrada'}
+def find_name(full_names)
+
+  "#{full_names[:first_name]} #{full_names[:middle_name]} #{full_names[:last_name]}"
+
+end
+
+puts find_name(names)
