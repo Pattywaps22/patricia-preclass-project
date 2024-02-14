9b3911f445e67d8f2e5b97c25694ff03dd15dc6c
AnnaPatricia <triciaentrada@gmail.com>
2024-02-14 17:13:33 +0800

    exercises_1-4

---
+++ exercises/exercise_1.rb
@@ -0,0 +1,8 @@
+def calculate_bmi(weight, height)
+  bmi = weight / height**2
+  return bmi
+end
+
+w = 65
+h = 1.63
+p calculate_bmi(w, h)
---
+++ exercises/exercise_2.rb
@@ -0,0 +1,11 @@
+
+def is_adult(age)
+  if age>=18
+    print "adult"
+  else
+    puts "not adult"
+  end
+end
+
+is_adult(17)
+is_adult(21)
